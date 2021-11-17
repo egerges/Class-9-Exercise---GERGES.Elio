b5ae2ce823b21ddf36e429279fd726104236a570
Elio Gerges <elio-gerges@outlook.com>
2021-11-17 17:00:17 -0500

    Implementing and testing diff function in file 02.

--- 01-unit/02-difference.js
+++ 01-unit/02-difference.js
@@ -1,8 +1,10 @@
+'USE STRICT';
+
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function difference() {
-
+function difference(x = 0, y = 0) {
+  return x - y;
 }
 
 // ==================================================
@@ -20,16 +22,22 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should ignore additional numbers.
+  var result = difference(1, 1, 3);
+  if (result !== 0) throw new Error('Expected difference(1, 1, 3) to be 0. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
+  var result = difference(1);
+  if (result !== 1) throw new Error('Expected difference(1) to be 1. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = difference();
+  if (result !== 0) throw new Error('Expected difference() to be 0. Received: ' + result);
 
   console.log('All tests passed successfully.');
 
@@ -40,3 +48,5 @@ try {
   console.warn('Whoops, the following test did not pass:');
   console.error(e.message);
 }
+
+module.exports = difference;
