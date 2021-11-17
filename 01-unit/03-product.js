960a53c13644da9f08edf1fd4979b246449601a8
Elio Gerges <elio-gerges@outlook.com>
2021-11-17 17:16:08 -0500

    Implementing and testing prod function in file 03.

--- 01-unit/03-product.js
+++ 01-unit/03-product.js
@@ -1,8 +1,16 @@
+'USE STRICT';
+
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function product() {
-
+function product(x, y) {
+  if (!x && !y) return 0;
+  if(y == 0) {
+    y = 0;
+  } else if (!y) {
+    y = 1;
+  }
+  return x * y;
 }
 
 // ==================================================
@@ -20,21 +28,29 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should return 0 when either of the numbers are 0.
+  var result = product(2, 0);
+  if (result !== 0) throw new Error('Expected product(2, 0) to be 0. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // It should ignore additional numbers.
+  var result = product(2, 2, 2);
+  if (result !== 4) throw new Error('Expected product(2, 2, 2) to be 4. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
+  var result = product(2);
+  if (result !== 2) throw new Error('Expected product(2) to be 2. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = product();
+  if (result !== 0) throw new Error('Expected product() to be 0. Received: ' + result);
 
   console.log('All tests passed successfully.');
 
@@ -45,3 +61,5 @@ try {
   console.warn('Whoops, the following test did not pass:');
   console.error(e.message);
 }
+
+module.exports = product;
