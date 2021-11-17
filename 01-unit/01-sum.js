ef94f616b02c12bd09379fc1c87883aa424f4272
Elio Gerges <elio-gerges@outlook.com>
2021-11-17 16:53:06 -0500

    Implementing and testing sum function in 01-sum.js

--- 01-unit/01-sum.js
+++ 01-unit/01-sum.js
@@ -1,8 +1,10 @@
+'USE STRICT';
+
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function sum() {
-
+function sum(x = 0, y = 0) {
+  return x + y;
 }
 
 // ==================================================
@@ -20,16 +22,22 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should ignore additional numbers.
+  var result = sum(1, 1, 3);
+  if (result !== 2) throw new Error('Expected sum(1, 1, 3) to be 2. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
+  var result = sum(1);
+  if (result !== 1) throw new Error('Expected sum(1) to be 1. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = sum();
+  if (result !== 0) throw new Error('Expected sum() to be 0. Received: ' + result);
 
   console.log('All tests passed successfully.');
 
@@ -40,3 +48,5 @@ try {
   console.warn('Whoops, the following test did not pass:');
   console.error(e.message);
 }
+
+module.exports = sum;
