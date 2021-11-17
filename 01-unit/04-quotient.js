cf771514bc1b437c2c62a7409d2a29d4f743b5ca
Elio Gerges <elio-gerges@outlook.com>
2021-11-17 17:28:19 -0500

    Implementing and testing quot function in file 04.

--- 01-unit/04-quotient.js
+++ 01-unit/04-quotient.js
@@ -1,8 +1,13 @@
+'USE STRICT';
+
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function quotient() {
-
+function quotient(x, y) {
+  if(y === 0) throw new Error('ERROR.');
+  if(!x && !y) return 0;
+  if(!y) return x;
+  return x/y;
 }
 
 // ==================================================
@@ -20,21 +25,33 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should return the text 'ERROR' when the second number is 0.
+  try {
+    var result = quotient(2, 0);
+    throw new Error('Expected quotient(2, 0) to throw an Error. Received: ' + result);
+  } catch (error) {
+    // Do nothing, test has passed.
+  }
 
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // It should ignore additional numbers.
+  var result = quotient(2, 2, 3);
+  if (result !== 1) throw new Error('Expected quotient(2, 2, 3) to be 1. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
+  var result = quotient(4);
+  if (result !== 4) throw new Error('Expected quotient(4) to be 4. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = quotient();
+  if (result !== 0) throw new Error('Expected quotient() to be 0. Received: ' + result);
 
   console.log('All tests passed successfully.');
 
@@ -45,3 +62,5 @@ try {
   console.warn('Whoops, the following test did not pass:');
   console.error(e.message);
 }
+
+module.exports = quotient;
